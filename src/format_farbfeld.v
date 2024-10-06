d2d7867e0bdd58d8657627c20bc6b08dd09d96f7
Ivan Vatlin <16368461+jeffangelion@users.noreply.github.com>
2024-10-06 21:48:40 +0300

    farbfeld: true -> big_endian in read_u16() calls

--- src/format_farbfeld.v
+++ src/format_farbfeld.v
@@ -7,17 +7,17 @@ pub struct FarbfeldOptions {}
 fn read_farbfeld(data []u8) (?Image, ?ImageOptions) {
 	mut offset := 8
 	mut image := Image{
-		width:  read_u32(data[offset..offset + 4], true)
-		height: read_u32(data[offset + 4..offset + 8], true)
+		width:  read_u32(data[offset..offset + 4], big_endian)
+		height: read_u32(data[offset + 4..offset + 8], big_endian)
 	}
 	image_options := ImageOptions(FarbfeldOptions{})
 	offset += 8
 	for offset < data.len {
 		image.pixels << Pixel(Truecolor{
-			red:   read_u16(data[offset..offset + 2], true)
-			green: read_u16(data[offset + 2..offset + 4], true)
-			blue:  read_u16(data[offset + 4..offset + 6], true)
-			alpha: read_u16(data[offset + 6..offset + 8], true)
+			red:   read_u16(data[offset..offset + 2], big_endian)
+			green: read_u16(data[offset + 2..offset + 4], big_endian)
+			blue:  read_u16(data[offset + 4..offset + 6], big_endian)
+			alpha: read_u16(data[offset + 6..offset + 8], big_endian)
 		})
 		offset += 8
 	}
@@ -27,29 +27,29 @@ fn read_farbfeld(data []u8) (?Image, ?ImageOptions) {
 fn write_farbfeld(image Image, image_options ImageOptions) []u8 {
 	mut data := []u8{cap: farbfeld_signature.len + 16 + image.pixels.len}
 	data << farbfeld_signature
-	data << write_u32(image.width, true)
-	data << write_u32(image.height, true)
+	data << write_u32(image.width, big_endian)
+	data << write_u32(image.height, big_endian)
 	for i := 0; i < image.pixels.len; i++ {
 		pixel := image.pixels[i]
 		match pixel {
 			Truecolor {
-				data << write_u16(pixel.red, true)
-				data << write_u16(pixel.green, true)
-				data << write_u16(pixel.blue, true)
-				data << write_u16(pixel.alpha, true)
+				data << write_u16(pixel.red, big_endian)
+				data << write_u16(pixel.green, big_endian)
+				data << write_u16(pixel.blue, big_endian)
+				data << write_u16(pixel.alpha, big_endian)
 			}
 			Grayscale {
-				data << write_u16(pixel.gray, true)
-				data << write_u16(pixel.gray, true)
-				data << write_u16(pixel.gray, true)
-				data << write_u16(pixel.alpha, true)
+				data << write_u16(pixel.gray, big_endian)
+				data << write_u16(pixel.gray, big_endian)
+				data << write_u16(pixel.gray, big_endian)
+				data << write_u16(pixel.alpha, big_endian)
 			}
 			Index {
 				palette_color := image.palette[pixel.index]
-				data << write_u16(palette_color.red, true)
-				data << write_u16(palette_color.green, true)
-				data << write_u16(palette_color.blue, true)
-				data << write_u16(palette_color.alpha, true)
+				data << write_u16(palette_color.red, big_endian)
+				data << write_u16(palette_color.green, big_endian)
+				data << write_u16(palette_color.blue, big_endian)
+				data << write_u16(palette_color.alpha, big_endian)
 			}
 		}
 	}
